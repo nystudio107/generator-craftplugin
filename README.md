43519b58592887216ab37d048f17e5338b7f6809
Andrew Welch <andrew@AmbrosiaSW.com>
2016-01-19 14:34:58 -0500

    Version 1.1.3

--- README.md
+++ README.md
@@ -172,6 +172,12 @@ The `craftplugin` generator can also be passed arguments via the command line, b
 
 ## Changelog
 
+### 1.1.2 -- 2016.01.20
+
+* Craft Commerce "Purchasables" now adds an ElementType, Model, and Record
+* Fixed an error in the naming of the plugin's Settings.twig template
+* Updated README.md
+
 ### 1.1.2 -- 2016.01.19
 
 * Added a Craft Commerce Purchasable ElementType template
---
+++ app/templates/api_version_2_5/records/_PurchasableRecord.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * <%= pluginName %> plugin for Craft CMS
+ *
+ * <%= pluginHandle %><%= purchasableName[index] %> Record
+ *
+ * --snip--
+ * Active record models (or “records”) are like models, except with a database-facing layer built on top. On top of
+ * all the things that models can do, records can:
+ *
+ * - Define database table schemas
+ * - Represent rows in the database
+ * - Find, alter, and delete rows
+ *
+ * Note: Records’ ability to modify the database means that they should never be used to transport data throughout
+ * the system. Their instances should be contained to services only, so that services remain the one and only place
+ * where system state changes ever occur.
+ *
+ * When a plugin is installed, Craft will look for any records provided by the plugin, and automatically create the
+ * database tables for them.
+ *
+ * https://craftcms.com/docs/plugins/records
+ * --snip--
+ *
+ * @author    <%= pluginAuthorName %>
+ * @copyright <%= copyrightNotice %>
+ * @link      <%= pluginAuthorUrl %>
+ * @package   <%= pluginHandle %>
+ * @since     <%= pluginVersion %>
+ */
+
+namespace Craft;
+
+class <%= pluginHandle %><%= purchasableName[index] %>Record extends BaseRecord
+{
+    /**
+     * Returns the name of the database table the model is associated with (sans table prefix). By convention,
+     * tables created by plugins should be prefixed with the plugin name and an underscore.
+     *
+     * @return string
+     */
+    public function getTableName()
+    {
+        return '<%= pluginDirName %><%= purchasableName[index] %>';
+    }
+
+    /**
+     * Returns an array of attributes which map back to columns in the database table.
+     *
+     * @access protected
+     * @return array
+     */
+   protected function defineAttributes()
+    {
+        return array(
+            'someField'     => array(AttributeType::String, 'default' => ''),
+        );
+    }
+
+    /**
+     * If your record should have any relationships with other tables, you can specify them with the
+     * defineRelations() function
+     * @return array
+     */
+    public function defineRelations()
+    {
+        return array(
+        );
+    }
+}
