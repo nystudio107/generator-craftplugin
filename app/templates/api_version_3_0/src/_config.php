05742d3ccf7fdea8baa9ac282d25e46646af7c7f
Andrew Welch <andrew@megalomaniac.com>
2017-01-28 21:53:38 -0500

    * Added config.php

---
+++ app/templates/api_version_3_0/src/_config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * <%= pluginName %> plugin for Craft CMS 3.x
+ *
+ * <%= pluginDescription %>
+ *
+ * @link      <%= pluginAuthorUrl %>
+ * @copyright <%= copyrightNotice %>
+ */
+
+/**
+ * <%= pluginName %> config.php
+ *
+ * Completely optional configuration settings for <%= pluginName %> if you want to customize some
+ * of its more esoteric behavior, or just want specific control over things.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as '<%= pluginDirName %>.php' and make
+ * your changes there.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as well, so you can
+ * have different settings groups for each environment, just as you do for 'general.php'
+ */
+
+return array(
+
+    // This controls blah blah blah
+    "someSetting" => true,
+
+);
