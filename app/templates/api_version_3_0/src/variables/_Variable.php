d3e7c77502ef61f28a98bc00f4b5501d6c011412
Dennis Gilli <dennis.gilli@acribis.ch>
2017-05-31 10:40:08 +0200

    Fixed typo in variables templates

--- app/templates/api_version_3_0/src/variables/_Variable.php
+++ app/templates/api_version_3_0/src/variables/_Variable.php
@@ -41,7 +41,7 @@ class <%= pluginHandle %>Variable
 
 <% if ((typeof codeComments !== 'undefined') && (codeComments)) { -%>
     /**
-     * Whatever you want to output to a Twig tempate can go into a Variable method.
+     * Whatever you want to output to a Twig template can go into a Variable method.
      * You can have as many variable functions as you want.  From any Twig template,
      * call it like this:
      *
