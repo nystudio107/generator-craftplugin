8e39b6b58423e9cc18e42776e9131aa8117d1198
lemiwinkz <giel@yellowflash.net>
2019-08-06 09:59:26 +0200

    Cleanup

--- app/templates/api_version_3_0/tests/_support/Helper/Functional.php
+++ app/templates/api_version_3_0/tests/_support/Helper/Functional.php
@@ -17,8 +17,6 @@ use Codeception\Module;
  *
  * Here you can define custom actions.
  * All public methods declared in helper class will be available in $I
- *
- * @since 3.2
  */
 class Functional extends Module
 {
