375cf132eb0fb174446a102e9320fd114fec13f2
Gilles FRANCOIS <gilles@sopress.net>
2020-01-31 14:04:18 +0100

    Since node 10, it is mandatory to pass a callback on fs.writefile() This was not the case on line 294 of the index.js file, so it was throwing the following error : 	Callback must be a function. Received 'utf8' I added an empty callback function to correct the problem.

--- app/index.js
+++ app/index.js
@@ -291,7 +291,7 @@ module.exports = yo.generators.Base.extend({
 
 /* -- Write the answers out to a JSON file */
 
-        fs.writeFile(this.destDir + PLUGIN_CONF_FILE_NAME, this.rawAnswers, "utf8");
+        fs.writeFile(this.destDir + PLUGIN_CONF_FILE_NAME, this.rawAnswers, "utf8", () => {});
         },
 
 /* -- writing -- Where you write the generator specific files (routes, controllers, etc) */
